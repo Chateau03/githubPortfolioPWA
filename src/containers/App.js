6d98fb54c4a6b9ac72a3ead1731cb95aba9cf15e
castlixx <raulincastle@gmail.com>
2020-10-30 16:06:17 -0500

    Styling in React with styled-components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Profile from "./Profile";
-import "./App.css";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Profile />
-    </div>
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <Header />
+        <Profile />
+      </AppWrapper>
+    </>
   );
 }
 
